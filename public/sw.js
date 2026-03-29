5fe024c73ea296197a94734d7eaf2ae8efe3c359
alvaro-leal-me <alvaro.leal@mindearth.ch>
2026-03-30 01:52:04 +0200

    Fix cache

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'blues-map-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'blues-map-v2';
 
-self.addEventListener('install', event => {
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/assets/')) {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return resp;
+      }))
+    );
+    return;
+  }
+  event.respondWith(fetch(event.request));
 });
